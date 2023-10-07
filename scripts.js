41a6a5ee77a1c24b0cb12a46d8e9510f0911f67f
lai_luv <l.lavaughn319@gmail.com>
2023-10-07 16:06:55 -0400

    done

--- scripts.js
+++ scripts.js
@@ -9,13 +9,13 @@ function authentication() {
 }
 
 console.log(numberOfUsers);
-console.log(numberOfAuthenticatedUsers);
+// console.log(numberOfAuthenticatedUsers);
 
 authentication();
 
 // Explain what will be logged in the console for each of the console logs in the block of code above.
 
-
+// the console log on line 7  and 11 are  console logging the global variable .while the console log on line 8 is console logging the local variable, But the console logs on line 7 and 8 won't run with out line 14 .       
 
 
 
@@ -25,28 +25,47 @@ authentication();
 
 let baseSalary = 50000;
 
-function calculateBonus() {
+
+function calculateBonus(salary) {
   let bonusPercentage = 0.1; // 10% bonus
-  return baseSalary * bonusPercentage;
+  return salary * bonusPercentage;
 }
 
-function calculateTotalSalary() {
-  let bonus = calculateBonus();
-  return baseSalary + bonus;
+function calculateTotalSalary(salary) {
+  let bonus = calculateBonus(salary);
+  return salary + bonus;
 }
 
 // Call the calculateTotalSalary function. Explain below each of the steps that are taken by the functions.
 
+calculateTotalSalary(5)
+console.log(calculateTotalSalary(25))
+// baseSalary is declared and assigned a value of 50000.
+
+// The calculateBonus function takes a salary argument, calculates a bonus of 10% of the salary, and returns the bonus amount.
+
+// The calculateTotalSalary function takes a salary argument, calls calculateBonus with the given salary, adds the bonus to the original salary, and returns the total salary.
+
+
+
 
 // What is the output?
 
-
+// 55,000
 
 
 // How would you change the functions so that instead of a global variable the function takes in an argument for the base salary?
 
 
+function calculateBonus(salary) {
+  let bonusPercentage = 0.1; // 10% bonus
+  return baseSalary * bonusPercentage;
+}
 
+function calculateTotalSalary(salary) {
+  let bonus = calculateBonus(salary);
+  return baseSalary + bonus;
+}
 
 
 
@@ -72,9 +91,11 @@ let user2 = {
 
 // Create a function that can take in a user variable. Return a string that says, "[NAME] lives in [CITY], [COUNTRY]"
 
-
-
-
+function userInfo(user){
+  return `${user.name} lives in ${user.address.city},${user.address.country}`
+}
+console.log(userInfo(user1))
+console.log(userInfo(user2))
 
 
 
@@ -110,8 +131,10 @@ function getNextBookToRead() {
 
 let nextBookInfo1 = getNextBookToRead(readingList);
 let nextBookInfo2 = getNextBookToRead(readingList2);
-
+console.log(nextBookInfo1)
+console.log(nextBookInfo2)
 // Run this code, then get nextBookInfo1 and nextBookInfo2 to log in the console. They are supposed
+
 // to be separate reading lists, but the function is always looking at what's in the first reading
 // list.
 
@@ -123,7 +146,17 @@ let nextBookInfo2 = getNextBookToRead(readingList2);
 // nextBookInfo2 are passing in the values for the reading lists they want to get the next book
 // from.
 
+function getNextBookToRead(books) {
+  let nextBook = books[0];
 
+  return (
+    "You should read " +
+    nextBook +
+    " next. You have " +
+    readingList.length +
+    " books left on your list!"
+  );
+}
 
 
 
@@ -133,9 +166,15 @@ let nextBookInfo2 = getNextBookToRead(readingList2);
 // Takes 1 argument: your dog's age.
 // Calculates your dog's age in humany years by multiplying the puppy’s age by 7.
 // Returns the result of a string like so: "Your doggie is NN years old in dog years!"
+function calculateDogAge(age){
+  let dogAge = prompt('How Many Human Years Has Your Dog Lived ?')
+  dogAge = dogAge*7
+  return `your dog is ${dogAge} years old in dog years!! `
+}
 
 // Call the function three times with different sets of values.
 
+console.log(calculateDogAge()) 
 
 
 
@@ -144,7 +183,7 @@ let nextBookInfo2 = getNextBookToRead(readingList2);
 // What type of scope are you using to declare the variables? 
 
 
-
+// local scope 
 
 
 /********************* Exercise 6  **********************/
@@ -159,6 +198,7 @@ showMessage("Dominique");
 
 // What would print in the alert message when the following code is executed? Think about it, then run the code to see.
 
-
+// Hello, dominique
 
 // How could we change the code so that we are alerting a message for Katherine? Don't write the string literal "Katherine" a second time (that value already exists if you want to use it again), and don't change the names of any current identifiers (i.e., variables and parameters)—username is a great name for the value globally and in the function!
+showMessage(username)
